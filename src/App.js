ae45f20322f38ca2f79d62c997dee316f1e5d190
Liu Tingodong <ltd0306@163.com>
2021-01-07 13:30:18 +0800

    add tour project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import Loading from './components/Loading';
+import Tours from './components/Tours'
+
+const url = 'https://course-api.com/react-tours-project'
 
 function App() {
+
+  // 初始化状态
+  const [tours, setTours] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  //请求数据
+  const fetchTours = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(url);
+      const tours = await response.json()
+      setLoading(false);
+      setTours(tours)
+    } catch (error) {
+      setLoading(false);
+      console.log(error)
+    }
+  } 
+
+  //useEffect
+  useEffect(() => {
+    fetchTours()
+  }, [])
+  console.log(tours)
+
+  //删除卡片
+  const removeTour = (id) => {
+    const newTours = tours.filter(tour => tour.id !== id )
+    setTours(newTours)
+  }
+
+  //判断加载
+  if(loading) {
+    return(
+      <main>
+        <Loading/>
+      </main>
+    )
+  }
+
+  //判断没有目的地信息
+  if(tours.length === 0) {
+    return(
+      <main>
+        <div className="title">
+          <h2>没有目的地信息</h2>
+          <button className="btn" onClick={() => fetchTours()}>重新获取旅游目的地信息</button>
+        </div>
+      </main>
+    )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <Tours tours = { tours } removeTour={removeTour}/>
+    </main>
   );
 }
 
